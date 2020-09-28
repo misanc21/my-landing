2dbe36684010bc7d2404c10c5bd87cf1a48b20bb
misanc21 <misanc21@gmail.com>
2020-09-28 12:51:06 -0500

    adding projects

--- src/helper/arrayProyectos.js
+++ src/helper/arrayProyectos.js
@@ -1,9 +1,90 @@
 
 export const proyectos = [
         {
+            id: 14,
+            nombre:'Product hunt clone',
+            descripcion: 'product hunt clone',
+            link: 'https://producthunt-next-hzd16k68l.vercel.app/',
+            madeWith:['Next', 'StyledComponents', 'Firebase'],
+            img: '../img/proyectos/mern-task.png',
+            altImg: 'Mern'
+        },
+        {
+            id: 12,
+            nombre:'Mern Tasks',
+            descripcion: 'To-do list with MERN tasks',
+            link: 'https://sharp-leakey-18b330.netlify.app/',
+            madeWith:['React', 'Bootswatch'],
+            img: '../img/proyectos/mern-task.png',
+            altImg: 'Mern'
+        },
+        {
+            id: 5,
+            nombre:'Qr Generator',
+            descripcion: 'Qr generator in pdf',
+            link: 'https://elated-fermat-32fd73.netlify.app/',
+            madeWith:['React', 'Express', 'Node', 'MaterialUI', 'StyledComponents'],
+            img: '../img/proyectos/form-qr.png',
+            altImg: 'Formulario qr'
+        },
+        {
+            id: 6,
+            nombre:'Dynamic Breadcrum',
+            descripcion: 'Dynamic breadcrumb made with react-router-dom hook',
+            link: 'https://eloquent-jennings-479c1a.netlify.app/',
+            madeWith:['React', 'Tailwind'],
+            img: '../img/proyectos/breadcrumb.png',
+            altImg: 'Dynamic breadcrumb'
+        },
+        {
+            id: 7,
+            nombre:'midpoint approximation form',
+            descripcion: 'Form to get the midpoint of an objective (test for employ)',
+            link: 'https://mystifying-boyd-f44b48.netlify.app/',
+            madeWith:['React', 'MaterialUI'],
+            img: '../img/proyectos/ketogains.png',
+            altImg: 'Midpoint'
+        },
+        {
+            id: 8,
+            nombre:'cryptocurrency trader',
+            descripcion: 'cryptocurrency trader form',
+            link: 'https://nervous-saha-f8d941.netlify.app/',
+            madeWith:['React', 'StyledComponents'],
+            img: '../img/proyectos/cotizador.png',
+            altImg: 'Trader'
+        },
+        {
+            id: 9,
+            nombre:'Recipes searcher',
+            descripcion: 'search drinks form',
+            link: 'https://wizardly-euler-083ff4.netlify.app/',
+            madeWith:['React', 'StyledComponents', 'MaterialUI'],
+            img: '../img/proyectos/bebidas.png',
+            altImg: 'bebidas'
+        },
+        {
+            id: 10,
+            nombre:'Lyrics searcher',
+            descripcion: 'search songs lyrics form',
+            link: 'https://condescending-feynman-076510.netlify.app/',
+            madeWith:['React', 'Bootswatch'],
+            img: '../img/proyectos/canciones.png',
+            altImg: 'bebidas'
+        },
+        {
+            id: 11,
+            nombre:'Images searcher',
+            descripcion: 'search images without copyright form',
+            link: 'https://xenodochial-wing-18caf0.netlify.app/',
+            madeWith:['React', 'Bootswatch'],
+            img: '../img/proyectos/imagenes.png',
+            altImg: 'imagenes'
+        },
+        {
             id: 1,
             nombre:'Arco+i ERP',
-            descripcion: 'ERP para control de despacho arquitectonico',
+            descripcion: 'architectural firm ERP',
             link: '',
             madeWith:['jQuery', 'PHP', 'MySql', 'Css3', 'Bootstrap'],
             img: '../img/proyectos/arco.png',
@@ -12,7 +93,7 @@ export const proyectos = [
         {
             id: 2,
             nombre:'Ruvini jeans ERP',
-            descripcion: 'ERP para control de fabrica de pantalones',
+            descripcion: 'Jeans factory ERP',
             link: '',
             madeWith:['jQuery', 'PHP', 'MySql', 'Css3', 'Bootstrap'],
             img: '../img/proyectos/ruv.png',
@@ -21,7 +102,7 @@ export const proyectos = [
         {
             id: 3,
             nombre:'Confecciones Ruver',
-            descripcion: 'Inventario para fabrica de ropa',
+            descripcion: 'clothes Factory inventory',
             link: '',
             madeWith:['Vue', 'Laravel', 'MySql', 'Css3', 'Bootstrap'],
             img: '../img/proyectos/confecciones-ruv.png',
@@ -30,19 +111,10 @@ export const proyectos = [
         {
             id: 4,
             nombre:'Maquila ruvinni',
-            descripcion: 'Control de sueldos por mano de obra de Maquila',
+            descripcion: 'Salary control in a factory',
             link: '',
             madeWith:['Vue', 'Laravel', 'MySql', 'Css3', 'Bootstrap'],
             img: '../img/proyectos/maquila-ruv.png',
             altImg: 'Maquila'
-        },
-        {
-            id: 4,
-            nombre:'Generador de Qr',
-            descripcion: 'Generador de reporte de Qr y datos',
-            link: 'https://elated-fermat-32fd73.netlify.app/',
-            madeWith:['React', 'Express', 'Node', 'MaterialUI', 'StyledComponents'],
-            img: '../img/proyectos/form-qr.png',
-            altImg: 'Formulario qr'
         },
     ]
